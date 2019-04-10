7e389d60593db98d1809ffc6105c77fb5d490ab3
zhengxidong <541410829@qq.com>
2019-04-10 22:52:48 +0800

    修改docker脚本

--- docker_image/run_web_blog_console_container.sh
+++ docker_image/run_web_blog_console_container.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 #停止容器
+echo "停止容器"
 docker stop web-blog-console
 #删除容器
+echo "删除容器"
 docker rm web-blog-console
 #删除镜像
 #docker rm web-blog-console
@@ -11,16 +13,18 @@ docker rm web-blog-console
 #./build_web_blog_console_image.sh
 
 # 切换目录
-cd ..
+#cd ..
 
 # 更新
+echo "更新代码"
 git pull
 
 # 实例化容器
-docker run -d --restart=always -p 7777:80 --name web-blog-console \
-	-v ./application:/usr/local/nginx/html/www.itellyou.site/application \
-	-v ./extend:/usr/local/nginx/html/www.itellyou.site/extend \
-	-v ./public:/usr/local/nginx/html/www.itellyou.site/public \
-	-v ./thinkphp:/usr/local/nginx/html/www.itellyou.site/thinkphp \
-	-v ./vendor:/usr/local/nginx/html/www.itellyou.site/vendor \
+docker run -d --restart=always -p 7070:80 --name web-blog-console \
+	-v /home/WebBlogConsole/application:/usr/local/nginx/html/console.itellyou.site/application \
+	-v /home/WebBlogConsole/extend:/usr/local/nginx/html/console.itellyou.site/extend \
+	-v /home/WebBlogConsole/public:/usr/local/nginx/html/console.itellyou.site/public \
+	-v /home/WebBlogConsole/thinkphp:/usr/local/nginx/html/console.itellyou.site/thinkphp \
+	-v /home/WebBlogConsole/runtime:/usr/local/nginx/html/console.itellyou.site/runtime \
+	-v /home/WebBlogConsole/vendor:/usr/local/nginx/html/console.itellyou.site/vendor \
 	web-blog-console
