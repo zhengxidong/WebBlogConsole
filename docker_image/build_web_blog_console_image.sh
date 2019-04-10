022e93cd1e386094a205bc79ac64360c304b66db
zhengxidong <541410829@qq.com>
2019-04-10 21:34:50 +0800

    删除文件

--- docker_image/build_web_blog_console_image.sh
+++ docker_image/build_web_blog_console_image.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build -t web-blog .
+docker build -t web-blog-console .
--- docker_image/run_web_blog_console_container.sh
+++ docker_image/run_web_blog_console_container.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 #停止容器
-docker stop web-blog
+docker stop web-blog-console
 #删除容器
-docker rm web-blog
+docker rm web-blog-console
 #删除镜像
-#docker rm web-blog
+#docker rm web-blog-console
 
 #重build镜像
-#./build_web_blog_image.sh
+#./build_web_blog_console_image.sh
 
 # 切换目录
 cd ..
@@ -17,10 +17,10 @@ cd ..
 git pull
 
 # 实例化容器
-docker run -d --restart=alwls -p 7777:80 --name web-blog \
+docker run -d --restart=always -p 7777:80 --name web-blog-console \
 	-v ./application:/usr/local/nginx/html/www.itellyou.site/application \
 	-v ./extend:/usr/local/nginx/html/www.itellyou.site/extend \
 	-v ./public:/usr/local/nginx/html/www.itellyou.site/public \
 	-v ./thinkphp:/usr/local/nginx/html/www.itellyou.site/thinkphp \
 	-v ./vendor:/usr/local/nginx/html/www.itellyou.site/vendor \
-	web-blog
+	web-blog-console
